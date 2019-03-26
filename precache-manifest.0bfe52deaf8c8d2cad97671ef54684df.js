e96c018a0fc294b227a7ad84966211208350b52d
grosjeanstephanie <grosjean.stephaniep@gmail.com>
2019-03-26 16:22:59 +0100

    Updates

---
+++ precache-manifest.0bfe52deaf8c8d2cad97671ef54684df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2b82258d943dd395690ec9e81869fa9",
+    "url": "/grosjeanstephanie/react-manager/static/media/super.f2b82258.jpg"
+  },
+  {
+    "revision": "70cfabc96126008db030",
+    "url": "/grosjeanstephanie/react-manager/static/js/runtime~main.fd5fcbc7.js"
+  },
+  {
+    "revision": "77304314d9bdad5e34c4",
+    "url": "/grosjeanstephanie/react-manager/static/js/main.d5630bdd.chunk.js"
+  },
+  {
+    "revision": "3f212a7ed54b5297e1f6",
+    "url": "/grosjeanstephanie/react-manager/static/js/2.e7da38ed.chunk.js"
+  },
+  {
+    "revision": "77304314d9bdad5e34c4",
+    "url": "/grosjeanstephanie/react-manager/static/css/main.5eeb2b4c.chunk.css"
+  },
+  {
+    "revision": "3f212a7ed54b5297e1f6",
+    "url": "/grosjeanstephanie/react-manager/static/css/2.dc5a1ff8.chunk.css"
+  },
+  {
+    "revision": "c80d1249a032dc62d015a5cbf4db4891",
+    "url": "/grosjeanstephanie/react-manager/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/grosjeanstephanie/react-manager/precache-manifest.0bfe52deaf8c8d2cad97671ef54684df.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/grosjeanstephanie/react-manager/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
